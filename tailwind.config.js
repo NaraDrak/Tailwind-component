18f1a0f8bb1d2daf41a24b8a5db80a07d622c032
Guillermo Villarreal Gallegos <memowars@outlook.es>
2021-02-04 21:26:52 -0600

    xD

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        maquetado: '#2bb3b1',
+        maquetado2: '#4abebd',
+        botoncolor: '#bfdf32'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
